a237b0373fb7ec386beb0b51f1d4616be533842a
c1ph3r.il <c1ph3r.il@9f8e2b2e-e8c3-11de-b92f-5d3588262692>
2010-03-14 20:53:48 +0000

    template grabber done

--- OpenCV/phase1/phase1/calibration.h
+++ OpenCV/phase1/phase1/calibration.h
@@ -8,4 +8,11 @@ void calibration(int board_w, int board_h, int n_boards, float square_size,
 /* this handles generation of perspective wrapping matrix */
 void birds_eye(int board_w, int board_h, CvSize resolution);
 
+/* this lets the user click on balls and save them as templates */
+void grab_templates(CvSize resolution);
+
+/* mouse callback wrapper for finding a ball around the mouse and saving it
+as a template */
+void saveTemplateAroundMouse(int event, int x, int y, int flags, void *param);
+
 #endif
