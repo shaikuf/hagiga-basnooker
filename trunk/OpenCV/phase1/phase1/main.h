d4b99118120bdb33663c5461477b29d7323b178b
c1ph3r.il <c1ph3r.il@9f8e2b2e-e8c3-11de-b92f-5d3588262692>
2010-03-14 20:33:02 +0000

    phase1 cleaned up

--- trunk/OpenCV/phase1/phase1/main.h
+++ trunk/OpenCV/phase1/phase1/main.h
@@ -1,21 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-/* Shows an image and forwared clicks on it to the given function until ESC is
-met */
-void genericMouseWrapper(IplImage *img, void (*func)(int, int, int, int, void *));
-
-/* Mouse callback wrapper that prints the color of the clicked point. param
-is the img to work on */
-void findColorAroundMouse(int event, int x, int y, int flags, void *param);
-
-/* Mouse callback wrapper that prints the position of the clicked point.
-param is the img to work on */
-void findPosAroundMouse(int event, int x, int y, int flags, void *param);
-
-/* Mouse callback wrapper for findBallAround. param is the img to work on */
-void findBallAroundMouse(int event, int x, int y, int flags, void *param);
-
 /* The main loop of the program */
 void gameLoop(CvSize resolution);
 
