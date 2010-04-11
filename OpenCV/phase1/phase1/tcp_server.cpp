845e411acbe0b485904a6438b22f5ee5b1c281b6
c1ph3r.il <c1ph3r.il@9f8e2b2e-e8c3-11de-b92f-5d3588262692>
2010-04-11 19:13:21 +0000

    forgot to add tcp server files :)

---
+++ OpenCV/phase1/phase1/tcp_server.cpp
@@ -0,0 +1,157 @@
+#include "tcp_server.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <errno.h>
+
+TCPServer::TCPServer(int port) {
+	// initialize the members
+	_port = port;
+	_client = 0;
+
+	// initialize winsock
+
+	WSADATA wsaData;   // if this doesn't work
+	//WSAData wsaData; // then try this instead
+
+		// MAKEWORD(1,1) for Winsock 1.1, MAKEWORD(2,0) for Winsock 2.0:
+
+	if (WSAStartup(MAKEWORD(1,1), &wsaData) != 0) {
+		fprintf(stderr, "WSAStartup failed.\n");
+		exit(1);
+	}
+
+	// initialize the server
+
+		// get us a socket and bind it
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	int retval;
+
+	ZeroMemory(&hints, sizeof (hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+		// Resolve the local address and port to be used by the server
+	char port_str[128];
+	sprintf(port_str, "%d", _port);
+	retval = getaddrinfo(NULL, port_str, &hints, &result);
+	if (retval != 0) {
+		printf("getaddrinfo failed: %d\n", retval);
+		WSACleanup();
+		exit(1);
+	}
+
+		// create socket
+	_listener = INVALID_SOCKET;
+	_listener = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	if (_listener == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		exit(1);
+	}
+
+		// bind socket
+	retval = bind( _listener, result->ai_addr, (int)result->ai_addrlen);
+	if (retval == SOCKET_ERROR) {
+		printf("bind failed: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(_listener);
+		WSACleanup();
+		exit(1);
+	}
+
+	// listen on socket
+	if ( listen( _listener, SOMAXCONN ) == SOCKET_ERROR ) {
+		printf( "Error at bind(): %ld\n", WSAGetLastError() );
+		closesocket(_listener);
+		WSACleanup();
+		exit(1);
+	}
+
+	// set socket to be non-blocking
+	u_long arg = 1;
+	ioctlsocket(_listener, FIONBIO, &arg);
+}
+
+TCPServer::~TCPServer() {
+	closesocket(_listener);
+
+	if(_client != 0)
+		closesocket(_client);
+
+	WSACleanup();
+}
+
+int TCPServer::update() {
+	if(_client == 0) {
+		// accept a client socket
+		_client = INVALID_SOCKET;
+		_client = accept(_listener, NULL, NULL);
+
+		if (_client == INVALID_SOCKET) {
+			if(WSAGetLastError() == WSAEWOULDBLOCK) {
+				// pass
+			} else {
+				printf("accept failed: %d\n", WSAGetLastError());
+			}
+
+			_client = 0;
+		}
+
+		return 0;
+	} else {
+		// try to receive
+
+		char buf[128];
+		int retval = recv(_client, buf, 128, 0);
+
+		if(retval == 0) {
+			closesocket(_client);
+			_client = 0;
+		} else if(retval == -1) {
+			if(WSAGetLastError() == WSAEWOULDBLOCK) {
+				// pass
+			} else {
+				printf("Error at recv(): %ld\n", WSAGetLastError());
+			}
+
+			return 0;
+		} else {
+			// received something
+			return 1;
+		}
+	}
+}
+
+void TCPServer::send_white_pos(float x, float y) {
+	if(_client == 0)
+		return;
+
+	char buf[128];
+	sprintf(buf, "%f\n%f\n", x, y);
+	if(send(_client, buf, strlen(buf), 0) == -1) {
+		printf("Error at send(): %ld\n", WSAGetLastError());
+		exit(1);
+	}
+}
+
+void TCPServer::send_theta(float theta) {
+	if(_client == 0)
+		return;
+
+	char buf[128];
+	sprintf(buf, "%f\n", theta);
+	if(send(_client, buf, strlen(buf), 0) == -1) {
+		printf("Error at send(): %ld\n", WSAGetLastError());
+		exit(1);
+	}
+}
+
---
+++ OpenCV/phase1/phase1/tcp_server.h
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+
+class TCPServer {
+	int _listener;
+	int _client;
+
+	int _port;
+
+public:
+	TCPServer(int port = 1234);
+	~TCPServer();
+
+	int update();
+
+	void send_white_pos(float x, float y);
+	void send_theta(float theta);
+};
