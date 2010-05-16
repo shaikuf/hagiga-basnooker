25c4298cf1db1299887d95fd6c8476b7115dc2ae
c1ph3r.il <c1ph3r.il@9f8e2b2e-e8c3-11de-b92f-5d3588262692>
2010-05-16 06:44:15 +0000

    guess i didn't resolve them very well :)

--- trunk/phase2/balls.h
+++ trunk/phase2/balls.h
@@ -4,12 +4,9 @@
 #include <cv.h>
 #include <vector>
 
+using namespace std;
+
 #define DEBUG_FIX_POS 0
-
-/* This finds the ball matching the given template on the image */
-void findBall(IplImage *img, IplImage *templ, CvPoint2D32f *center,
-			  float *radius);
-
 #define BALL_CORR_THD 0.85
 
 /* This finds the balls matching the given template on the image */
